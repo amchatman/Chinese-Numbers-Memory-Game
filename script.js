3527a7d8f9d1d5b0d7d1c5359dcc5f079569466f
Ashley Chatman <ashchat2008@gmail.com>
2023-02-13 13:20:22 -0500

    Array -Add script -Create array

---
+++ script.js
@@ -0,0 +1,22 @@
+/*Create a board with 20 blocks
+Store 10 blocks with Chinenes numbers
+The other 10 have the romanization pinyin
+Each card must match  the chinese number with the 
+romaization.
+If cards match add point if cards dont match reflip card
+When all card been match message congulation and reset
+On Reset rerange cards and play again.
+*/
+
+//Create a array to store characters
+const chineseNumbers = ['一','二','三','四','五','六','七','八','九','十'];
+const chinesePinyin =['yì','èr','sān','sì','wǔ','liù','qī','bā,','jiǔ','shí'];
+
+//Go through the length of the array
+for(let i = 0; i < chineseNumbers.length; i++){
+    console.log(chineseNumbers[i]);
+}
+for(let j = 0; j < chinesePinyin.length; j++){
+    console.log(chinesePinyin[j]);
+}
+console.log('Hello');
