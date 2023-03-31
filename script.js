cb0628718160c43653da56685f7bcc313db3f2b6
Ashley Chatman <ashchat2008@gmail.com>
2023-03-31 09:40:37 -0400

    Alert -Update alert menu

--- script.js
+++ script.js
@@ -76,8 +76,16 @@ function cardFlip(){
                     cardTwo = [];
                     matchedPairs++;
                     if (matchedPairs === chineseNumbers.length / 2) {
-                        // All pairs have been matched, show message
-                        alert('Congratulations, you have matched all the pairs!');
+                        const alertMessage = document.createElement('div');
+                            alertMessage.innerHTML = "Congulation you match all pairs!";
+
+                        const refreshButton = document.createElement('button');
+                            refreshButton.innerHTML = 'Play Again?';
+                            refreshButton.addEventListener('click',function(){
+                                location.reload();
+                            });
+                            alertMessage.appendChild(refreshButton);
+                            alert(alertMessage.innerHTML);
                     }
                 } else {
                     // The cards do not match, flip them back over
